0cc1df75319197547d5aa317632bc2f15f04dac9
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2023-01-06 18:27:21 -0600

    better solution to gas problem with comments

--- coding_problems/gas_problem.py
+++ coding_problems/gas_problem.py
@@ -56,10 +56,40 @@ def gas_station(gas, cost):
     return -1
 
 
-print(gas_station(gas, cost))
+# print(gas_station(gas, cost))
 
 # this one was much more of a challenge, I initially wanted to use recursion
 # to solve the problem passing the updated start and calling the function again
 # had to look to the solution to get the problem right after thinking about it for a
 # a while. I will have to revisit this one and see if i can make it work in another
 # way to mix it up some.
+
+def gas_station_2(gas, cost):
+    current_gas = 0
+    previous_gas = 0
+    current_station = 0
+    for i in range(len(gas)):
+        current_gas += gas[i] - cost[i]
+        if current_gas < 0:
+            current_gas = 0
+            current_station = i + 1
+            previous_gas += current_gas
+    if current_station == len(gas) or current_gas + previous_gas < 0:
+        return -1
+    else:
+        return current_station
+
+print(gas_station_2(gas, cost))
+
+# this is the better solution, But i will still have to wrap my head around whats
+# going on here. I understand that were skipping the stations where we run out of
+# since we know that all stations in between cannot be that answer, but im still
+# a little unclear as to why we know we can go from the end all the way back around
+# again
+#
+# we use the previous gas amount as a check to see if there will be a < 0
+# gas amount after reaching the end because this would indicate that if you used
+# the gas you had at the end of the array to start from the start again we would still
+# run out along the way thus there is no station that is valid otherwise the
+# current station is returned as the valid station
+#
