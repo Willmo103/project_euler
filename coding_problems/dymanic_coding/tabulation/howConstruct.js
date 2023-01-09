5ac4c69c8c2820508006f64a139e79357f04aa08
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2023-01-09 02:00:32 -0600

    more competed

--- coding_problems/dymanic_coding/tabulation/howConstruct.js
+++ coding_problems/dymanic_coding/tabulation/howConstruct.js
@@ -1,19 +1,22 @@
 const howConstruct = (target, words) => {
-  const table = Array(target.length + 1).fill(false);
-  table[0] = true;
+  const table = Array(target.length + 1).fill(0);
+  table[0] = 1;
   for (let i = 0; i < target.length; i++) {
-    if (table[i] === true) {
+    if (table[i] !== 0) {
       for (let word of words) {
         if (target.slice(i, i + word.length) === word) {
-          table[i + word.length] = true;
+          table[i + word.length] += table[i];
         }
       }
     }
   }
+  console.log(table);
   return table[target.length];
 };
+
 console.log(howConstruct("abcdefgh", ["ab", "c", "def", "gh"]));
 console.log(howConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "bo"]));
+console.log(howConstruct("purple", ["purp", "p", "ur", "le", "purpl"]));
 console.log(
   howConstruct("enterpotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])
 );
@@ -28,4 +31,5 @@ console.log(
   ])
 );
 
-// got most of the way there on my own before going to the answer and checking my work
+// actually wrote the whole working thing without looking!! this one felt more
+// fluid now that im starting to understand how this maps out
