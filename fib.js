dbc05f654c6ac1829025163bcb547e4b140222eb
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2023-01-09 17:03:03 -0600

    dumb mistakes were made

--- fib.js
+++ fib.js
@@ -11,9 +11,11 @@ const fib = (n) => {
 const fibMemo = (n, memo = {}) => {
   if (n in memo) return memo[n];
   if (n <= 2) return 1;
-  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  memo[n] = fibMemo(n - 1, memo) + fibMemo(n - 2, memo);
+  //   memo[n] = fib(n - 1, memo) + fib(n - 2, memo); // < --- was calling fib() instead of fibMemo()
   return memo[n];
 };
+
 console.log(fibMemo(7));
 console.log(fibMemo(12));
 console.log(fibMemo(9));
@@ -22,3 +24,5 @@ console.log(fibMemo(50));
 // cannot figure out why all of the sudden this same logic in JS is no longer working
 // memo only updates on last call of function and stores no values along the way
 // preventing the memo from being useful
+
+// It helps if i call the same function as in fibMemo, instead of fib in my memoized calls... dumb mistake
